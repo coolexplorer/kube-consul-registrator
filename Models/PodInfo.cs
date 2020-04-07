833350ec9a93da87ed24abc1adb7b33ffefdde75
seunkim <seunkim@ea.com>
2020-04-07 16:49:29 +0900

    Fixed pod port parsing issues

--- Models/PodInfo.cs
+++ Models/PodInfo.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using k8s.Models;
 
 namespace kube_consul_registrator.Models
 {
@@ -7,7 +8,7 @@ namespace kube_consul_registrator.Models
         public string Name { get; set; }
         public string NodeName { get; set; }
         public string Ip { get; set; }
-        public int Port { get; set; }
+        public IList<V1Container> Containers { get; set; }
         public string Phase { get; set; }
         public IDictionary<string, string> Annotations { get; set; }
     }
