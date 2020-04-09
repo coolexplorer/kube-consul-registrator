9b4a11b018646c99483cc8418dec7fe0ae88ee36
seunkim <seunkim@ea.com>
2020-04-09 19:43:41 +0900

    Added unit test

--- kube-consul-registrator.Tests/KubernetesControllerTest.cs
+++ kube-consul-registrator.Tests/KubernetesControllerTest.cs
@@ -7,6 +7,7 @@ using kube_consul_registrator.Const;
 using Moq;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using System.Threading.Tasks;
 
 namespace kube_consul_registrator.Tests
 {
@@ -31,7 +32,26 @@ namespace kube_consul_registrator.Tests
             Assert.Equal(400, badRequestResult.StatusCode);
         }
 
-        private List<PodInfo> GetTestPods()
+        [Fact]
+        public void GetPods_OkResult_WhenModelStateIsValid()
+        {
+            // Arrage
+            var mockRepo = new Mock<IKubernetesRepository>();
+            var mockLogger = new Mock<ILogger<KubernetesController>>();
+            mockRepo.Setup(repo => repo.GetPods("default")).Returns(Task.FromResult(GetTestPods()));
+            var controller = new KubernetesController(mockRepo.Object, mockLogger.Object);
+
+            // Act
+            var task = controller.GetPods("default");
+            task.Wait();
+
+            // Assert
+            var okResult = Assert.IsType<OkObjectResult>(task.Result);
+            var model = Assert.IsAssignableFrom<List<PodInfo>>(okResult.Value);
+            Assert.Equal(2, model.Count);
+        }
+
+        private IEnumerable<PodInfo> GetTestPods()
         {
             return new List<PodInfo>()
             {
